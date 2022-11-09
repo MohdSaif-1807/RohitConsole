b12b5161bc1b02ad3549fa5401f316750b555a2f
MohdSaif-1807 <saifmohd1807@gmail.com>
2022-11-09 10:33:23 +0530

    this is rohit project

---
+++ src/main/java/in/kmit/Rohit/App.java
@@ -0,0 +1,13 @@
+package in.kmit.Rohit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is Rrohithh's Maven Project" );
+    }
+}
